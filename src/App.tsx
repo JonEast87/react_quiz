23cf7521e539dac9422871a0a6dee82c486c3c92
JonEast87 <j.d.east@icloud.com>
2024-01-13 16:00:56 -0700

    scaffolding and setting up QuestionCard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
+// Components
+import QuestionCard from "./components/QuestionCard";
 
-function App() {
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
-    <div className="App">Quiz</div>
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score: </p>
+      <p className="">Loading Questions ...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
   );
-}
+};
 
 export default App;
