45eed435484f09dd65245efbbe01ea62fd19c488
JonEast87 <j.d.east@icloud.com>
2024-01-14 09:34:12 -0700

    implemented check answer logic, will now work on next question logic

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
-  correntAnswer: string;
+  correctAnswer: string;
 };
 
 const TOTAL_QUESTIONS = 10;
@@ -38,7 +38,24 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      // Users answer
+      const answer = e.currentTarget.value;
+      // Check answer against correct value
+      const correct = questions[number].correct_answer === answer;
+      // Add score if answer is correct
+      if (correct) setScore((prev) => prev + 1);
+      // Save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
 
   const nextQuestion = () => {};
 
